e5f0dd7ba323e6a3bc735602254ec861f722a204
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-04-15 03:54:23 -0300

    Get id from route

--- src/app/senator-information/senator-information.component.ts
+++ src/app/senator-information/senator-information.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { API } from '../services/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'senator-information',
   templateUrl: './senator-information.component.html',
   styleUrls: ['./senator-information.component.scss'],
 })
-export class SenatorInformationComponent {
-  constructor(private API: API) {}
+export class SenatorInformationComponent implements OnInit {
+  constructor(private API: API, private route: ActivatedRoute) {}
+  id: number;
+  ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+  }
 }
